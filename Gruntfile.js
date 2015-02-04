9f61c7fa484a2b22486e37a3d735de1fff2b6e20
hschuetz <henning.schuetz@devbliss.com>
2015-02-04 15:34:29 +0100

    further project setup, grunt build now working

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,12 +3,12 @@
 module.exports = function (grunt) {
     require('load-grunt-tasks')(grunt);
     grunt.loadNpmTasks('grunt-devbliss');
-    
+
     grunt.initConfig({
-      devbliss: {
-          port: 8081,
-          testport: 9091,
-          livereload: 9999
-      },
-    });       
+        devbliss: {
+            port: 8081,
+            testport: 9091,
+            livereload: 9999
+        },
+    });
 };
