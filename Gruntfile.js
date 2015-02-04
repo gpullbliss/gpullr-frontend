6a1eac40ecd8b29025ce6b0e839732ebeae4946d
hschuetz <henning.schuetz@devbliss.com>
2015-02-04 13:06:25 +0100

    initial setup part 2

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,4 +3,28 @@
 module.exports = function (grunt) {
     require('load-grunt-tasks')(grunt);
     grunt.loadNpmTasks('grunt-devbliss');
+    
+    grunt.initConfig({
+      devbliss: {
+          port: 8081,
+          testport: 9091,
+          livereload: 9999
+      },
+    });    
+    
+    grunt.registerTask('build', [
+        //'test',
+        'jshint',
+        'clean',
+        'copy',
+        'less',
+        'bowerInstall',
+        'useminPrepare',
+        'concat',
+        'uglify',
+        'cssmin',
+        'rev',
+        'usemin',
+        'htmlmin'
+    ]);    
 };
