82a361658b3c2f493c55689a7fed3b268f71f1c2
Michael Diodone <michael.diodone@devbliss.com>
2015-02-13 11:04:56 +0100

    Implemented dashboard against fake backend, should be complete

--- app/app_components/dashboardModule/controllers/dashboardCtrl.js
+++ app/app_components/dashboardModule/controllers/dashboardCtrl.js
@@ -1,33 +1,27 @@
 'use strict';
 angular.module('dashboardModule')
-    .controller('dashboardCtrl', ['$scope', function ($scope) {
-        $scope.pullRequestCollection = [
-            {
-                title: 'Update 3001-angularjs-styleguide.md',
-                url: 'https://github.com/devbliss/manuals/pull/44',
-                repository: 'manuals',
-                author: {
-                    name: 'Ömer Karahan',
-                    avatarUrl: 'https://avatars2.githubusercontent.com/u/3127128?v=3'
-                },
-                creationDate: '2015-02-11T12:12:31Z',
-                filesChanged: 1,
-                linesAdded: 112,
-                linesRemoved: 0,
-                status: 'Merged'
-            }, {
-                title: 'refactor/testSourceSets',
-                url: 'https://github.com/devbliss/ecosystem-course-aggregation/pull/49',
-                repository: 'ecosystem-course-aggregation',
-                author: {
-                    name: 'Elena Shafranova',
-                    avatarUrl: 'https://avatars3.githubusercontent.com/u/1777303?v=3'
-                },
-                creationDate: '2015-02-11T13:12:31Z',
-                filesChanged: 15,
-                linesAdded: 334,
-                linesRemoved: 313,
-                status: 'Open'
+    .controller('dashboardCtrl', ['$scope', '$interval', 'pullRequestService', function ($scope, $interval, pullRequestService) {
+        var updatePullRequestsInterval,
+            getPullRequests;
+
+        getPullRequests = function () {
+            pullRequestService.getPullRequests()
+                .then(function (pullRequests) {
+                    $scope.pullRequests = pullRequests;
+                });
+        };
+
+        getPullRequests();
+
+        if (angular.isUndefined(updatePullRequestsInterval)) {
+            updatePullRequestsInterval = $interval(getPullRequests, 60000);
+        }
+
+        $scope.$on('$destroy', function () {
+                if (angular.isDefined(updatePullRequestsInterval)) {
+                    $interval.cancel(updatePullRequestsInterval);
+                    updatePullRequestsInterval = undefined;
+                }
             }
-        ];
+        );
     }]);
