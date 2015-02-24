9b72573ca93dcc8b1d7a50a4c59072089d581aea
Michael Diodone <michael.diodone@devbliss.com>
2015-02-24 10:02:02 +0100

    Fixed indention

--- test/spec/app_components/dashboardModule/services/pullRequestServiceSpec.js
+++ test/spec/app_components/dashboardModule/services/pullRequestServiceSpec.js
@@ -25,40 +25,40 @@ describe('pullRequestService', function () {
             responseData =
             {
                 items: [
-                {
-                    id: 12345,
-                    title: 'Update 3001-angularjs-styleguide.md',
-                    url: 'https://github.com/devbliss/manuals/pull/44',
-                    repository: 'manuals',
-                    author: {
-                        username: 'Ömer Karahan',
-                        avatarUrl: 'https://avatars2.githubusercontent.com/u/3127128?v=3'
-                    },
-                    creationDate: '2015-02-11T12:12:31Z',
-                    filesChanged: 1,
-                    linesAdded: 112,
-                    linesRemoved: 0,
-                    assignee: null,
-                    status: 'Merged'
-                }, {
-                    id: 12345,
-                    title: 'refactor/testSourceSets',
-                    url: 'https://github.com/devbliss/ecosystem-course-aggregation/pull/49',
-                    repository: 'ecosystem-course-aggregation',
-                    author: {
-                        username: 'Elena Shafranova',
-                        avatarUrl: 'https://avatars3.githubusercontent.com/u/1777303?v=3'
-                    },
-                    creationDate: '2015-02-11T13:12:31Z',
-                    filesChanged: 15,
-                    linesAdded: 334,
-                    linesRemoved: 313,
-                    assignee: {
-                        username: 'marcelb',
-                        avatarUrl: 'https://avatars1.githubusercontent.com/u/308374?v=3'
-                    },
-                    status: 'Open'
-                }
+                    {
+                        id: 12345,
+                        title: 'Update 3001-angularjs-styleguide.md',
+                        url: 'https://github.com/devbliss/manuals/pull/44',
+                        repository: 'manuals',
+                        author: {
+                            username: 'Ömer Karahan',
+                            avatarUrl: 'https://avatars2.githubusercontent.com/u/3127128?v=3'
+                        },
+                        creationDate: '2015-02-11T12:12:31Z',
+                        filesChanged: 1,
+                        linesAdded: 112,
+                        linesRemoved: 0,
+                        assignee: null,
+                        status: 'Merged'
+                    }, {
+                        id: 12345,
+                        title: 'refactor/testSourceSets',
+                        url: 'https://github.com/devbliss/ecosystem-course-aggregation/pull/49',
+                        repository: 'ecosystem-course-aggregation',
+                        author: {
+                            username: 'Elena Shafranova',
+                            avatarUrl: 'https://avatars3.githubusercontent.com/u/1777303?v=3'
+                        },
+                        creationDate: '2015-02-11T13:12:31Z',
+                        filesChanged: 15,
+                        linesAdded: 334,
+                        linesRemoved: 313,
+                        assignee: {
+                            username: 'marcelb',
+                            avatarUrl: 'https://avatars1.githubusercontent.com/u/308374?v=3'
+                        },
+                        status: 'Open'
+                    }
                 ]
             };
 
