6d353d31804993cbe28794bcbdcaba40cb3e67ac
Alexander Bluem <alexander.bluem@devbliss.com>
2015-03-02 12:15:52 +0100

    color by age

--- app/app_components/dashboardModule/directives/pullRequestDirective.js
+++ app/app_components/dashboardModule/directives/pullRequestDirective.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('dashboardModule')
-.directive('pullRequest', function () {
+.directive('pullRequest', function (moment) {
     return {
         scope: {
             pr: '=prdata'
@@ -9,10 +9,33 @@ angular.module('dashboardModule')
         restrict: 'E',
         templateUrl: 'app_components/dashboardModule/views/pullRequest.html',
         link: function(scope, element) {
-            element.css({
-                backgroundColor: 'red',
-                display: 'block'
-            });
+
+            var changeColor = function(){
+                var diff = moment().diff(scope.pr.createdAt, 'minutes');
+                var colorClass;
+
+                // hours difference rounds up and down. therefore once above the round up threshold, apply rule.
+                if (diff < 90) {
+                    colorClass = 'youngerThan2h';
+
+                } else if (diff >= 90 && diff < 210) {
+                    colorClass = 'olderThan2h';
+
+                } else if (diff >= 210 && diff < 450) {
+                    colorClass = 'olderThan4h';
+
+                } else if (diff >= 450 && diff < 43170) {
+                    colorClass = 'olderThan8h';
+
+                } else if (diff >= 43170) {
+                    colorClass = 'olderThanAMonth';
+                }
+
+                element.addClass(colorClass);
+            };
+
+            changeColor();
+
         }
     };
 });
