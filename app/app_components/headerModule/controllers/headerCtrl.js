ab194addc5c601ccb2c6d985fcb1ab7f3fbc7320
Philipp Karstedt <philipp.karstedt@devbliss.com>
2015-03-11 11:56:19 +0100

    added Ranking to header navigation and implemented navigation logic

--- app/app_components/headerModule/controllers/headerCtrl.js
+++ app/app_components/headerModule/controllers/headerCtrl.js
@@ -1,9 +1,19 @@
 'use strict';
 angular.module('headerModule')
-    .controller('headerCtrl', ['$scope', '$rootScope', 'userService', function ($scope, $rootScope, userService) {
+    .controller('headerCtrl', ['$scope', '$rootScope', 'userService', '$state', 'STATE_STATS', 'STATE_DASHBOARD',
+                      function ($scope, $rootScope, userService, $state, STATE_STATS, STATE_DASHBOARD) {
         
         $scope.userPresent = false;
         $scope.countPresent = false;
+        
+        var navBar = [
+              { title: 'All Requests', bubble: true, selected: 'active'},
+              { title: 'Ranking', bubble: false, selected: ''}
+            ],
+            initNavBar = [
+              { title: 'All Requests', bubble: true, selected: 'active'},
+              { title: 'Ranking', bubble: false, selected: ''}
+            ];
         
         var requestCountEvent = $rootScope.$on('changeRequestCount', function (event, data) {
                       $scope.requestCount = data ;
@@ -15,6 +25,32 @@ angular.module('headerModule')
                       $scope.avatarUrl = data.avatarUrl;
                       $scope.userPresent = true;
         });
+        
+        $scope.navigate = function (selected) {
+            if (selected === undefined) {
+                $scope.navBar = initNavBar;
+                $state.go(STATE_DASHBOARD);
+                return;
+            }
+
+            angular.forEach(navBar, function (item) {
+               if (item.title === selected.title) {
+                   item.selected='active';
+               } else {
+                   item.selected='';
+               }
+            });
+            $scope.navBar = navBar;
+            switch (selected.title) {
+                case 'All Requests':
+                   $state.go(STATE_DASHBOARD);
+                   break;
+                case 'Ranking':
+                   $state.go(STATE_STATS);
+            }
+        }
+        
+        $scope.navBar = navBar;
         
         $scope.$on('$destroy', requestCountEvent);
         $scope.$on('$destroy', updatedUser);
