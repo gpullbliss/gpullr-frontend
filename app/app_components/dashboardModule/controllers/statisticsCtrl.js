b9a049ca376e2704db95c42590633d1d224a3aab
Philipp Karstedt <philipp.karstedt@devbliss.com>
2015-03-11 10:19:54 +0100

    removed id from tabObject using queryParam instead

--- app/app_components/dashboardModule/controllers/statisticsCtrl.js
+++ app/app_components/dashboardModule/controllers/statisticsCtrl.js
@@ -3,10 +3,10 @@
 angular.module('dashboardModule')
        .controller('statisticsCtrl', ['$scope', 'userService', function($scope, userService) {
        var defaultTabs = [
-                    {id: 1, qp: 'today', title: 'Day', selected: 'active'},
-                    {id: 2, qp: 'last_7_days', title: 'Week', selected: ''},
-                    {id: 3, qp: 'last_30_days', title: 'Month', selected: ''},
-                    {id: 4, qp: 'all_time', title: 'All time', selected: ''}
+                    {qp: 'today', title: 'Day', selected: 'active'},
+                    {qp: 'last_7_days', title: 'Week', selected: ''},
+                    {qp: 'last_30_days', title: 'Month', selected: ''},
+                    {qp: 'all_time', title: 'All time', selected: ''}
                 ];
                 
         $scope.getScopedRankingList = function (selectedTab) {
@@ -16,7 +16,7 @@ angular.module('dashboardModule')
         var getScopedRankingList = function (selectedTab) {
             console.log(selectedTab);
             angular.forEach(defaultTabs, function (tab) {
-               if (tab.id === selectedTab.id) {
+               if (tab.qp === selectedTab.qp) {
                    tab.selected='active';
                } else {
                    tab.selected='';
