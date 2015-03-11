10555bb7e8850315a0d936b2c81a7ab77580a5c9
Philipp Karstedt <philipp.karstedt@devbliss.com>
2015-03-11 17:56:07 +0100

    removed periods log

--- app/app_components/dashboardModule/controllers/statisticsDetailsCtrl.js
+++ app/app_components/dashboardModule/controllers/statisticsDetailsCtrl.js
@@ -2,7 +2,6 @@
 
 angular.module('dashboardModule')
        .controller('statisticsDetailsCtrl', ['$scope', 'userService', '$state', function($scope, userService, $state) {
-        console.log($state.current.data.period);
 
         var currentPeriod = $state.current.data.period;
         
