5ef6ad63fe410c65f3426fec10f23bbe435b93fa
Philipp Karstedt <philipp.karstedt@devbliss.com>
2015-03-11 10:20:16 +0100

    refactored module config for new statistics screen

--- app/app_components/dashboardModule/config/routeConfig.js
+++ app/app_components/dashboardModule/config/routeConfig.js
@@ -1,13 +1,20 @@
 'use strict';
 angular.module('dashboardModule')
     .constant('STATE_DASHBOARD', 'dashboard')
+    .constant('STATE_STATS', 'stats')
     .config([
-        '$stateProvider', 'STATE_DASHBOARD', function ($stateProvider, STATE_DASHBOARD) {
+        '$stateProvider', 'STATE_DASHBOARD', 'STATE_STATS', function ($stateProvider, STATE_DASHBOARD, STATE_STATS) {
             $stateProvider
                 .state(STATE_DASHBOARD, {
                     parent: 'page',
                     controller: 'dashboardCtrl',
                     templateUrl: 'app_components/dashboardModule/views/dashboard.html',
                     url: '/'
+                })
+                .state(STATE_STATS, {
+                parent: 'pageWithoutHeader',
+                controller: 'statisticsCtrl',
+                templateUrl: 'app_components/dashboardModule/views/statistics.html',
+                url: '/' + STATE_STATS
                 });
         }]);
