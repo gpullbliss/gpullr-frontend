4eb4666656779d9eae4f35c53a2f2109b8a8185f
Philipp Karstedt <philipp.karstedt@devbliss.com>
2015-03-12 15:16:51 +0100

    fixed review comments

--- app/app_components/headerModule/controllers/headerCtrl.js
+++ app/app_components/headerModule/controllers/headerCtrl.js
@@ -5,28 +5,23 @@ angular.module('headerModule')
         function ($scope, $rootScope, userService, STATE_STATS, STATE_DASHBOARD) {
 
             $scope.userPresent = false;
-            $scope.countPresent = false;
 
             var navBar = [
                 {title: 'All Requests', bubble: true, state: STATE_DASHBOARD},
                 {title: 'Ranking', bubble: false, state: STATE_STATS}
             ];
 
-            var requestCountEvent = $rootScope.$on('changeRequestCount', function (event, data) {
+            $rootScope.$on('changeRequestCount', function (event, data) {
                 $scope.requestCount = data;
-                $scope.countPresent = true;
             });
 
-            var updatedUser = $rootScope.$on('updateUser', function (event, data) {
+            $rootScope.$on('updateUser', function (event, data) {
                 $scope.username = data.username;
                 $scope.avatarUrl = data.avatarUrl;
                 $scope.userPresent = true;
             });
 
             $scope.navBar = navBar;
-
-            $scope.$on('$destroy', requestCountEvent);
-            $scope.$on('$destroy', updatedUser);
 
             userService.whoAmI();
         }]);
