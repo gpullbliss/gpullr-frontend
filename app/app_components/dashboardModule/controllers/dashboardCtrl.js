094013c985c40038cf4c45a02f9c0d866f30f6ff
Philipp Karstedt <philipp.karstedt@devbliss.com>
2015-03-16 12:18:19 +0100

    added missing semicolon

--- app/app_components/dashboardModule/controllers/dashboardCtrl.js
+++ app/app_components/dashboardModule/controllers/dashboardCtrl.js
@@ -43,7 +43,7 @@ angular.module('dashboardModule')
                 userSettingsService.persistOrderSettings(user).then(function (res) {
                     getPullRequests();
                 });
-            }
+            };
 
         }]
 );
