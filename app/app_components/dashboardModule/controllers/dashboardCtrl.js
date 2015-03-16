d702bcf9b781d86a4971eef836db4be6791e1a05
Daniel Walldorf <daniel.walldorf@devbliss.com>
2015-03-16 11:05:50 +0100

    dashboardCtrl functionality to sort list of prs

--- app/app_components/dashboardModule/controllers/dashboardCtrl.js
+++ app/app_components/dashboardModule/controllers/dashboardCtrl.js
@@ -1,27 +1,49 @@
 'use strict';
 angular.module('dashboardModule')
-    .controller('dashboardCtrl', ['$scope', '$rootScope', '$interval', 'pullRequestService', function ($scope, $rootScope, $interval, pullRequestService) {
-        var updatePullRequestsInterval,
-            getPullRequests;
-        
-        getPullRequests = function () {
-            pullRequestService.getPullRequests().then(function (pullRequests) {
-                $scope.pullRequests = pullRequests;
-                $rootScope.$emit('changeRequestCount', pullRequests.length);
-            });
-        };
+    .controller('dashboardCtrl', ['$scope', '$rootScope', '$interval', 'pullRequestService', 'UserSettingsService',
+        function ($scope, $rootScope, $interval, pullRequestService, userSettingsService) {
 
-        getPullRequests();
+            var updatePullRequestsInterval,
+                getPullRequests;
 
-        updatePullRequestsInterval = $interval(getPullRequests, 60000);
-        
-        $scope.$on('$destroy', function () {
-                $interval.cancel(updatePullRequestsInterval);
-                updatePullRequestsInterval = undefined;
-            }
-        );
+            getPullRequests = function () {
+                pullRequestService.getPullRequests().then(function (pullRequests) {
+                    $scope.pullRequests = pullRequests;
+                    $rootScope.$emit('changeRequestCount', pullRequests.length);
+                });
+            };
 
-        $rootScope.$on('changeAssignee', function () {
             getPullRequests();
-        });
-    }]);
+
+            updatePullRequestsInterval = $interval(getPullRequests, 60000);
+
+            $scope.$on('$destroy', function () {
+                    $interval.cancel(updatePullRequestsInterval);
+                    updatePullRequestsInterval = undefined;
+                }
+            );
+
+            $rootScope.$on('changeAssignee', function () {
+                getPullRequests();
+            });
+
+            $rootScope.$on('updateUser', function (event, user) {
+                $scope.user = user;
+            });
+
+            $scope.orderPrList = function (sortOrder) {
+                var user = angular.copy($scope.user);
+                if (user.userSettingsDto === null) {
+                    user.userSettingsDto = {
+                        orderOptionDto: sortOrder
+                    };
+                } else {
+                    user.userSettingsDto.orderOptionDto = sortOrder;
+                }
+                userSettingsService.persistOrderSettings(user).then(function (res) {
+                    getPullRequests();
+                });
+            }
+
+        }]
+);
