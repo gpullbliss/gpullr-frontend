24c4657a05d5eef2a2a57518a7dfdf0de07d3fe4
Tobias Arndt <tobias.arndt@devbliss.com>
2015-03-18 14:16:25 +0100

    add layout for repo filter list

--- app/app_components/headerModule/controllers/headerCtrl.js
+++ app/app_components/headerModule/controllers/headerCtrl.js
@@ -1,11 +1,12 @@
 'use strict';
 angular.module('headerModule')
     /* jshint maxparams:false */
-    .controller('headerCtrl', ['$scope', '$rootScope', 'userService', 'STATE_STATS', 'STATE_DASHBOARD',
-        function ($scope, $rootScope, userService, STATE_STATS, STATE_DASHBOARD) {
+    .controller('headerCtrl', ['$scope', '$rootScope', 'userService', 'STATE_STATS', 'STATE_DASHBOARD', 'STATE_REPO_FILTER',
+        function ($scope, $rootScope, userService, STATE_STATS, STATE_DASHBOARD, STATE_REPO_FILTER) {
             $scope.navBar = [
                 {title: 'All Requests', bubble: true, state: STATE_DASHBOARD},
-                {title: 'Ranking', bubble: false, state: STATE_STATS}
+                {title: 'Ranking', bubble: false, state: STATE_STATS},
+                {title: 'Settings', bubble: false, state: STATE_REPO_FILTER}
             ];
 
             $rootScope.$on('changeRequestCount', function (event, requestCount) {
