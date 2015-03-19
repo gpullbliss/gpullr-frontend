476c40cbf23bd38c88dab164718d04d6e59318fe
Philipp Karstedt <philipp.karstedt@devbliss.com>
2015-03-19 17:26:09 +0100

    pimped rankingList markup for statistics details

---
+++ test/spec/app_components/dashboardModule/directives/rankingDirectiveSpec.js
@@ -0,0 +1,51 @@
+'use strict';
+
+describe('directive: pullrequest', function () {
+    var $compile,
+        $scope,
+        momentMock = {
+            diff: function () {
+            }
+        };
+
+    function getDirectiveHtml(pr) {
+        var html = '<pull-request class="block margin hPadding" prdata="{createdAt: '  + pr.createdAt + '}"></pull-request>';
+               console.log(html);
+               
+        return html;
+    }
+
+    beforeEach(function () {
+        var moment = function () {
+            return momentMock;
+        };
+
+        spyOn(momentMock, 'diff').and.callFake(function (timestamp, unit) {
+            return timestamp;
+        });
+
+        module('dashboardModule', function ($provide) {
+            $provide.value('moment', moment);
+        });
+        module('appTemplates');
+
+        inject(function (_$compile_, _$rootScope_) {
+            $compile = _$compile_;
+            $scope = _$rootScope_.$new();
+        });
+    });
+
+    describe('pull requests', function () {
+        it('sets the class youngerThan2h for a 89 minutes old pull request', function () {
+            var pr = {createdAt: 89},
+                element = $compile(getDirectiveHtml(pr))($scope);
+
+            $scope.$digest();
+            // TODO extra test
+            console.log(element.html());
+            expect(momentMock.diff).toHaveBeenCalledWith(pr.createdAt, 'minutes');
+
+            expect(element.attr('class')).toContain('youngerThan2h');
+        });
+    });
+});
