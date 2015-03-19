1c48f7f52d17e86a44039f882fe3370d28365daa
Michael Diodone <michael.diodone@devbliss.com>
2015-03-19 12:50:51 +0100

    Created pullRequestCssClassService

---
+++ app/app_components/pullRequestModule/services/pullRequestCssClassService.js
@@ -0,0 +1,28 @@
+'use strict';
+angular.module('pullRequestModule')
+    .factory('pullRequestCssClassService', ['moment', function (moment) {
+        function getColorClassDependingOnAge(dateTime, prefix) {
+            var colorClass,
+                minutesDiff = moment().diff(dateTime, 'minutes');
+            // hours difference rounds up and down. therefore once above the round up threshold, apply rule.
+            if (minutesDiff < 90) {
+                colorClass = 'youngerThan2h';
+            } else if (minutesDiff >= 90 && minutesDiff < 210) {
+                colorClass = 'olderThan2h';
+            } else if (minutesDiff >= 210 && minutesDiff < 450) {
+                colorClass = 'olderThan4h';
+            } else if (minutesDiff >= 450 && minutesDiff < 43170) {
+                colorClass = 'olderThan8h';
+            } else if (minutesDiff >= 43170) {
+                colorClass = 'olderThanAMonth';
+            }
+            if (prefix) {
+                colorClass = prefix + colorClass.charAt(0).toUpperCase() + colorClass.slice(1);
+            }
+            return colorClass;
+        }
+
+        return {
+            getColorClassDependingOnAge: getColorClassDependingOnAge
+        };
+    }]);
