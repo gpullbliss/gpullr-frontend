933997da6ed359f5aadd25f8a73755ec6bc9ec2d
Philipp Karstedt <philipp.karstedt@devbliss.com>
2015-03-20 17:34:31 +0100

    fixed test by providing pullRequestCssClassService for pullRequestDiective

--- test/spec/app_components/dashboardModule/directives/pullRequestDirectiveSpec.js
+++ test/spec/app_components/dashboardModule/directives/pullRequestDirectiveSpec.js
@@ -4,14 +4,7 @@ describe('directive: pullrequest', function () {
     var $compile,
         $scope,
         $cssColorClass = 'youngerThan2h',
-        pullRequestCssClassServiceMock = {
-            getColorClassDependingOnAge: function() {}
-        },
-        momentMock = {
-            diff: function () {
-            }
-        }
-        ;
+        pullRequestCssClassService;
 
     function getDirectiveHtml(pr) {
         var html = '<pull-request class="block margin hPadding" prdata="{createdAt: '  + pr.createdAt + '}"></pull-request>';
@@ -19,30 +12,21 @@ describe('directive: pullrequest', function () {
     }
 
     beforeEach(function () {
-        var pullRequestCssClassService = function () {
-                return pullRequestCssClassServiceMock;
-            },
-            moment = function () {
-                return momentMock;
-            };
-
-        spyOn(momentMock, 'diff').and.callFake(function (timestamp) {
-            console.log('momentmock fake call');
-            return timestamp; 
-        });
-
-        spyOn(pullRequestCssClassServiceMock, 'getColorClassDependingOnAge').and.callFake(function (blbla) {
-            console.log(blbla);
-            console.log('in getColorCss fake call in test');
-            return 'arschloch';
-        });
-
-        module('dashboardModule', function ($provide) {
-            $provide.value('_pullRequestCssClassService_', pullRequestCssClassService);
-            $provide.value('moment', moment);
-        });
-        
+        module('dashboardModule');
         module('appTemplates');
+        
+        pullRequestCssClassService = {
+             getColorClassDependingOnAge: function () {
+             }
+        };
+
+        spyOn(pullRequestCssClassService, 'getColorClassDependingOnAge').and.callFake(function () {
+            return $cssColorClass;
+        });
+
+        module('pullRequestModule', function ($provide) {
+            $provide.value('pullRequestCssClassService', pullRequestCssClassService);
+        });
 
         inject(function (_$compile_, _$rootScope_) {
             $compile = _$compile_;
@@ -51,14 +35,13 @@ describe('directive: pullrequest', function () {
     });
 
     describe('pull requests', function () {
-        it('sets the class youngerThan2h for a 89 minutes old pull request', function () {
+        it('sets the specified class for a 89 minutes old pull request', function () {
             var pr = {createdAt: 89},
                 element = $compile(getDirectiveHtml(pr))($scope);
 
             $scope.$digest();
 
-            expect(pullRequestCssClassServiceMock.getColorClassDependingOnAge).toHaveBeenCalledWith(89);
-            expect(momentMock.diff).toHaveBeenCalledWith(pr.createdAt, 'minutes');
+            expect(pullRequestCssClassService.getColorClassDependingOnAge).toHaveBeenCalledWith(pr.createdAt);
             expect(element.attr('class')).toContain($cssColorClass);
         });
     });
