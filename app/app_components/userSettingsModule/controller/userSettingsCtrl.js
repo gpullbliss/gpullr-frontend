2b4f7a1667e83f4896ee7bf7e6c92580149768fa
Daniel Walldorf <daniel.walldorf@devbliss.com>
2015-03-20 15:07:10 +0100

    updating blacklist

--- app/app_components/userSettingsModule/controller/userSettingsCtrl.js
+++ app/app_components/userSettingsModule/controller/userSettingsCtrl.js
@@ -1,21 +1,38 @@
 'use strict';
 
 angular.module('userSettingsModule')
-    .controller('userSettingsCtrl', ['$scope', '$state', 'repoService',
-        function ($scope, $state, repoService) {
-            repoService.getRepoList().then(function(repos){
-                $scope.repos = repos;
-            }),
+    .controller('userSettingsCtrl', ['$rootScope', '$scope', '$state', 'repoService',
+        function ($rootScope, $scope, $state, repoService) {
+
+            function init() {
+                repoService.getRepoList().then(function (repos) {
+                    $scope.repos = repos;
+                });
+
+                $scope.blacklist = [];
+            }
+
             $scope.toggle = function () {
-                $('.toggle').click(function() {
+                $('.toggle').click(function () {
                     var thisElement = $(this);
-                    if (thisElement.hasClass('showInList')){
+                    if (thisElement.hasClass('showInList')) {
                         thisElement.removeClass('showInList').addClass('hideInList');
                     } else {
                         thisElement.removeClass('hideInList').addClass('showInList');
                     }
                 });
             };
+
+            $scope.updateBlacklist = function (repoId) {
+                if ($scope.blacklist.indexOf(repoId) === -1) {
+                    $scope.blacklist.push(repoId);
+                } else {
+                    $scope.blacklist.splice($scope.blacklist.indexOf(repoId), 1);
+                }
+                console.log($scope.blacklist);
+            };
+
+            init();
         }
     ]
 );
