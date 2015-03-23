0aa868ed8705d75eaee6e73b4a53cfe96a1d2612
Michael Diodone <michael.diodone@devbliss.com>
2015-03-23 12:34:37 +0100

    Renamed prdata/pr to pullRequest, refactored test

--- app/app_components/dashboardModule/directives/pullRequestDirective.js
+++ app/app_components/dashboardModule/directives/pullRequestDirective.js
@@ -1,43 +1,42 @@
 'use strict';
 
 angular.module('dashboardModule')
-.directive('pullRequest', function (pullRequestCssClassService) {
-    return {
-        scope: {
-            pr: '=prdata'
-        },
-        restrict: 'E',
-        controller: function ($scope, pullRequestService) {
-            var currentPr = 'undefined';
-            $scope.assignToMe = function (selectedPr) {
-                currentPr = selectedPr;
-                if (currentPr.assignee === null) {
+    .directive('pullRequest', ['pullRequestCssClassService', function (pullRequestCssClassService) {
+        return {
+            scope: {
+                pullRequest: '='
+            },
+            restrict: 'E',
+            controller: function ($scope, pullRequestService) {
+                var currentPr = 'undefined';
+                $scope.assignToMe = function (selectedPr) {
+                    currentPr = selectedPr;
+                    if (currentPr.assignee === null) {
+                        pullRequestService.assignPullRequest(currentPr.id);
+                    } else {
+                        $scope.modalShown = true;
+                    }
+                };
+
+                $scope.confirmAssignment = function () {
                     pullRequestService.assignPullRequest(currentPr.id);
-                } else {
-                    $scope.modalShown = true;
-                }
-            };
-            
-            $scope.confirmAssignment = function () {
-                pullRequestService.assignPullRequest(currentPr.id);
-                currentPr = 'undefined';
-                $scope.modalShown = false;
-            };
-        
-            $scope.abortAssignment = function () {
-                currentPr = 'undefined';
-                $scope.modalShown = false;
-            };
-        
-            $scope.modalClose = function () {
-                $scope.modalShown = false;
-            };
-        },
-        templateUrl: 'app_components/dashboardModule/views/pullRequest.html',
-        link: function(scope, element) {
-            console.log(pullRequestCssClassService);
-            var colorClass = pullRequestCssClassService.getColorClassDependingOnAge(scope.pr.createdAt);
-            element.addClass(colorClass);
-        }
-    };
-});
+                    currentPr = 'undefined';
+                    $scope.modalShown = false;
+                };
+
+                $scope.abortAssignment = function () {
+                    currentPr = 'undefined';
+                    $scope.modalShown = false;
+                };
+
+                $scope.modalClose = function () {
+                    $scope.modalShown = false;
+                };
+            },
+            templateUrl: 'app_components/dashboardModule/views/pullRequest.html',
+            link: function (scope, element) {
+                var colorClass = pullRequestCssClassService.getColorClassDependingOnAge(scope.pullRequest.createdAt);
+                element.addClass(colorClass);
+            }
+        };
+    }]);
--- test/spec/app_components/dashboardModule/directives/pullRequestDirectiveSpec.js
+++ test/spec/app_components/dashboardModule/directives/pullRequestDirectiveSpec.js
@@ -1,31 +1,29 @@
 'use strict';
 
-describe('directive: pullrequest', function () {
+describe('pullRequest', function () {
     var $compile,
         $scope,
         $cssColorClass = 'youngerThan2h',
         pullRequestCssClassService;
 
-    function getDirectiveHtml(pr) {
-        var html = '<pull-request class="block margin hPadding" prdata="{createdAt: '  + pr.createdAt + '}"></pull-request>';
+    function getDirectiveHtml(createdAt) {
+        var html = '<pull-request class="block margin hPadding" pull-request="{createdAt: ' + createdAt + '}"></pull-request>';
         return html;
     }
 
     beforeEach(function () {
-        module('dashboardModule');
+        module('dashboardModule', function ($provide) {
+            $provide.value('pullRequestCssClassService', pullRequestCssClassService);
+        });
         module('appTemplates');
         
         pullRequestCssClassService = {
-             getColorClassDependingOnAge: function () {
-             }
+            getColorClassDependingOnAge: function () {
+            }
         };
 
         spyOn(pullRequestCssClassService, 'getColorClassDependingOnAge').and.callFake(function () {
             return $cssColorClass;
-        });
-
-        module('pullRequestModule', function ($provide) {
-            $provide.value('pullRequestCssClassService', pullRequestCssClassService);
         });
 
         inject(function (_$compile_, _$rootScope_) {
@@ -36,12 +34,12 @@ describe('directive: pullrequest', function () {
 
     describe('pull requests', function () {
         it('sets the specified class for a 89 minutes old pull request', function () {
-            var pr = {createdAt: 89},
-                element = $compile(getDirectiveHtml(pr))($scope);
+            var createdAt = 89,
+                element = $compile(getDirectiveHtml(createdAt))($scope);
 
             $scope.$digest();
 
-            expect(pullRequestCssClassService.getColorClassDependingOnAge).toHaveBeenCalledWith(pr.createdAt);
+            expect(pullRequestCssClassService.getColorClassDependingOnAge).toHaveBeenCalledWith(createdAt);
             expect(element.attr('class')).toContain($cssColorClass);
         });
     });
