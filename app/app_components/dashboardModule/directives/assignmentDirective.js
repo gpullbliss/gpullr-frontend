0f4a3331fd60759fbcbb8751338886870ebf857f
Philipp Karstedt <philipp.karstedt@devbliss.com>
2015-03-31 11:34:07 +0200

    added tests for assignmentDirectiveController

--- app/app_components/dashboardModule/directives/assignmentDirective.js
+++ app/app_components/dashboardModule/directives/assignmentDirective.js
@@ -11,12 +11,12 @@ angular.module('dashboardModule')
             controller: function ($scope, pullRequestService) {
                 var ACTION_ASSIGN_TO_ME = 'assignToMe',
                     ACTION_UNASSIGN_ME = 'unassignMe',
-                    ACTION_CONFIRM_ASSIGN_TO_ME = 'confirmAassignToMe',
-                    currentPr = 'undefined',
+                    ACTION_CONFIRM_ASSIGN_TO_ME = 'confirmAssignToMe',
+                    currentPr,
                     defaultTitle = 'Assign myself';
                  
                 function init() {
-                    if ($scope.pullRequest.assignee === null) {
+                    if (!$scope.pullRequest.assignee) {
                         $scope.assignTitle= defaultTitle;
                         $scope.assignment = ACTION_ASSIGN_TO_ME;
                         $scope.assignmentStyle = '';
@@ -48,18 +48,13 @@ angular.module('dashboardModule')
 
                 $scope.confirmAssignment = function () {
                     pullRequestService.assignPullRequest(currentPr.id);
-                    currentPr = 'undefined';
                     $scope.modalShown = false;
                 };
 
                 $scope.abortAssignment = function () {
-                    currentPr = 'undefined';
                     $scope.modalShown = false;
                 };
 
-                $scope.modalClose = function () {
-                    $scope.modalShown = false;
-                };
                 init();
             },
             templateUrl: 'app_components/dashboardModule/views/assignment.html'
