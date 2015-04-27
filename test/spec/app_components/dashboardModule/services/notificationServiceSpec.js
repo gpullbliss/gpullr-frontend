d8bf12de603a3e5db99c8964620c252ae57b5825
Alexander Blüm <abluem@alex3000.local>
2015-04-27 16:10:36 +0200

    notification service test

--- test/spec/app_components/dashboardModule/services/notificationServiceSpec.js
+++ test/spec/app_components/dashboardModule/services/notificationServiceSpec.js
@@ -2,13 +2,14 @@
 
 describe('notificationService', function () {
     var httpBackend,
-        service;
+        service,
+        response;
 
     beforeEach(function () {
         module('dashboardModule');
 
         inject(function (notificationService, _$httpBackend_) {
-            service = notificationService
+            service = notificationService;
             httpBackend = _$httpBackend_;
         });
     });
@@ -19,39 +20,58 @@ describe('notificationService', function () {
     });
 
     var expectedUrl = '/api/notifications',
-        successPayload =            {
-            'items': [
-                {
-                    'id': 1,
-                    'createdAt': '2015-04-17T14:55:59Z',
-                    'type': 'PULLREQUEST_CLOSED',
-                    'actorName': 'u1',
-                    'repoTitle': 'repo1',
-                    'pullRequestTitle': 'feature1'
-                },
-                {
-                    'id': 2,
-                    'createdAt': '2015-04-17T14:56:00Z',
-                    'type': 'PULLREQUEST_CLOSED',
-                    'actorName': 'u2',
-                    'repoTitle': 'repo2',
-                    'pullRequestTitle': 'feature2'
-                },
-                {
-                    'id': 3,
-                    'createdAt': '2015-04-17T14:55:59Z',
-                    'type': 'PULLREQUEST_CLOSED',
-                    'actorName': 'u1',
-                    'repoTitle': 'repo1',
-                    'pullRequestTitle': 'feature3'
-                }
-            ]
-        };
-    ;
+        successPayload = [
+            {
+                'id': 1,
+                'createdAt': '2015-04-17T14:55:59Z',
+                'type': 'PULLREQUEST_CLOSED',
+                'actorName': 'u1',
+                'repoTitle': 'repo1',
+                'pullRequestTitle': 'feature1'
+            },
+            {
+                'id': 2,
+                'createdAt': '2015-04-17T14:56:00Z',
+                'type': 'PULLREQUEST_CLOSED',
+                'actorName': 'u2',
+                'repoTitle': 'repo2',
+                'pullRequestTitle': 'feature2'
+            },
+            {
+                'id': 3,
+                'createdAt': '2015-04-17T14:55:59Z',
+                'type': 'PULLREQUEST_CLOSED',
+                'actorName': 'u1',
+                'repoTitle': 'repo1',
+                'pullRequestTitle': 'feature3'
+            }
+        ];
 
-    describe('getNotifications', function () {
-        beforeEach(function () {
-            response = httpBackend.expectGET(expectedUrl).respond(successPayload);
+    describe('notification service', function () {
+
+        it('calls correct URL for retrieval of notifications', function () {
+            var result;
+            httpBackend.expectGET(expectedUrl).respond(successPayload);
+
+            service.getNotifications().then(function (response) {
+                result = response;
+            });
+            httpBackend.flush();
+
+            expect(result).toEqual(successPayload.items);
+        });
+
+        it('calls correct URL for removal of selected single notifications', function () {
+            httpBackend.expectDELETE(expectedUrl + '/1').respond();
+            service.markNotificationRead(1);
+            httpBackend.flush();
+        });
+
+        it('calls correct URL for removal all notifications', function () {
+            httpBackend.expectDELETE(expectedUrl).respond();
+            service.markAllNotificationsRead();
+
+            httpBackend.flush();
         });
 
     })
