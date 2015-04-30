dd9a8cbeb3332e8773d95c63f9a72def407371f4
Daniel Walldorf <daniel.walldorf@devbliss.com>
2015-04-30 15:48:01 +0200

    adaptive colors

--- app/app_components/dashboardModule/directives/pullRequestDirective.js
+++ app/app_components/dashboardModule/directives/pullRequestDirective.js
@@ -5,13 +5,21 @@ angular.module('dashboardModule')
         return {
             scope: {
                 pullRequest: '=',
+                ageProperty: '=',
                 loggedInUser: '='
             },
             restrict: 'A',
             templateUrl: 'app_components/dashboardModule/views/pullRequest.html',
-            controller:['$scope', 'UserNameService', function($scope, userNameService){$scope.getName = userNameService.getName;}],
+            controller: ['$scope', 'UserNameService', function ($scope, userNameService) {
+                $scope.getName = userNameService.getName;
+            }],
             link: function (scope, element) {
-                var colorClass = pullRequestCssClassService.getColorClassDependingOnAge(scope.pullRequest.createdAt);
+                var propertyName = scope.ageProperty;
+                var age = scope.pullRequest[propertyName];
+
+                console.log(propertyName + ": " + age);
+
+                var colorClass = pullRequestCssClassService.getColorClassDependingOnAge(age);
                 element.addClass(colorClass);
             }
         };
