b44552a6d97fbee601a17301b23ee736745e6860
Daniel Walldorf <daniel.walldorf@devbliss.com>
2015-05-04 12:08:02 +0200

    now using user configured language

--- app/app_components/loginModule/services/userService.js
+++ app/app_components/loginModule/services/userService.js
@@ -1,55 +1,71 @@
 'use strict';
 angular.module('loginModule')
-    .factory('userService', ['$cacheFactory', '$http', '$rootScope', function ($cacheFactory, $http, $rootScope) {
-        function getCurrentUser() {
-            return $http.get('/api/users/me', {cache: true}).then(
-                function (response) {
-                    $rootScope.user = response.data;
-                    return response.data;
-                }
-            );
-        }
+    .factory('userService', ['$cacheFactory', '$http', '$rootScope', '$translate',
+        function ($cacheFactory, $http, $rootScope, $translate) {
 
-        function getUsersForLogin() {
-            return $http.get('/api/users').then(
-                function (response) {
-                    return response.data;
-                }
-            );
-        }
-        function getLanguages() {
-            return $http.get('api/users/languages').then(
-                function (response) {
-                    return response.data;
-                }
-            );
-        }
+            function getCurrentUser() {
+                return $http.get('/api/users/me', {cache: true}).then(
+                    function (response) {
+                        $rootScope.user = response.data;
+                        setUserLanguage($rootScope.user);
 
-        function logInUser(user) {
-            var successfulResponseStatus = 201;
-            clearCacheForGetCurrentUser();
-
-            return $http.post('/api/users/login/' + user.id, '').then(
-                function (response) {
-                    if (response.status !== successfulResponseStatus) {
-                        throw 'Got response code ' + response.status + ' instead of ' + successfulResponseStatus;
-                    } else {
-                        getCurrentUser();
+                        return response.data;
                     }
+                );
+            }
+
+            function getUsersForLogin() {
+                return $http.get('/api/users').then(
+                    function (response) {
+                        return response.data;
+                    }
+                );
+            }
+
+            function getLanguages() {
+                return $http.get('api/users/languages').then(
+                    function (response) {
+                        return response.data;
+                    }
+                );
+            }
+
+            function logInUser(user) {
+                var successfulResponseStatus = 201;
+                clearCacheForGetCurrentUser();
+
+                return $http.post('/api/users/login/' + user.id, '').then(
+                    function (response) {
+                        if (response.status !== successfulResponseStatus) {
+                            throw 'Got response code ' + response.status + ' instead of ' + successfulResponseStatus;
+                        } else {
+                            getCurrentUser();
+                        }
+                    }
+                );
+            }
+
+            function clearCacheForGetCurrentUser() {
+                $cacheFactory.get('$http').remove('/api/users/me');
+            }
+
+            return {
+                getCurrentUser: getCurrentUser,
+                getUsersForLogin: getUsersForLogin,
+                logInUser: logInUser,
+                getLanguages: getLanguages,
+
+                clearCacheForGetCurrentUser: clearCacheForGetCurrentUser
+            };
+
+
+            // private methods
+            function setUserLanguage(user) {
+                console.log('setting user configured language');
+                var userSettings = user.userSettingsDto;
+                if (userSettings && userSettings.language) {
+                    $translate.use(userSettings.language);
                 }
-            );
-        }
-
-        function clearCacheForGetCurrentUser() {
-            $cacheFactory.get('$http').remove('/api/users/me');
-        }
-
-        return {
-            getCurrentUser: getCurrentUser,
-            getUsersForLogin: getUsersForLogin,
-            logInUser: logInUser,
-            getLanguages: getLanguages,
-
-            clearCacheForGetCurrentUser: clearCacheForGetCurrentUser
-        };
-    }]);
+            }
+        }]
+);
