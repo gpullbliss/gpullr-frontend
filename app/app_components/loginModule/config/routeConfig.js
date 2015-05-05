25a4dfdebfda9d644358a0d33ef282b78b08af01
Marco Reinwarth <marco.reinwarth@devbliss.com>
2015-05-05 11:21:43 +0200

    added new route config

--- app/app_components/loginModule/config/routeConfig.js
+++ app/app_components/loginModule/config/routeConfig.js
@@ -2,12 +2,17 @@
 angular.module('loginModule')
     .constant('STATE_LOGIN', 'login')
     .config([
-        '$stateProvider', 'STATE_LOGIN', function ($stateProvider, STATE_LOGIN) {
+        '$stateProvider', 'STATE_LOGIN', 'STATE_OAUTH_GITHUB', function ($stateProvider, STATE_LOGIN, STATE_OAUTH_GITHUB) {
             $stateProvider
                 .state(STATE_LOGIN, {
                     parent: 'page',
                     controller: 'loginCtrl',
                     templateUrl: 'app_components/loginModule/views/login.html',
                     url: '/login'
+                }).state(STATE_OAUTH_GITHUB, {
+                    parent: 'page',
+                    controller: 'oauthCtrl',
+                    templateUrl: 'app_components/loginModule/views/oauth.html',
+                    url: '/oauth/callback'
                 });
         }]);
