5cec9e6d9661ebccd8e4ca810aa0f5fdd5445a39
Alexander Blüm <alexander.bluem@devbliss.com>
2015-05-07 11:52:09 +0200

    cleaned up statistics details controller

--- app/app_components/dashboardModule/controllers/statisticsDetailsCtrl.js
+++ app/app_components/dashboardModule/controllers/statisticsDetailsCtrl.js
@@ -3,24 +3,30 @@
 angular.module('dashboardModule')
     .controller('statisticsDetailsCtrl', ['$scope', '$state', 'statisticsService', 'UserNameService', function ($scope, $state, statisticsService, userNameService) {
 
-        var currentPeriod = $state.current.data.period;
+        function init() {
+            var currentPeriod = $state.current.data.period;
+            var extractFirstThreeRanks = function (rankedItems, podium) {
+                // ASSUME SORTED INPUT; BACKEND PROVIDES IT
+                angular.forEach(rankedItems, function (rankListItem) {
+                    if (rankListItem.rank > 3) {
+                        return;
+                    }
+                    if (this[rankListItem.rank] === undefined) {
+                        this[rankListItem.rank] = [];
+                    }
+                    this[rankListItem.rank].push(rankListItem.user);
+                }, podium);
+            };
 
-        $scope.getName = userNameService.getName;
-        $scope.podium = {};
+            $scope.getName = userNameService.getName;
+            $scope.podium = {};
 
-        statisticsService.getRankingList(currentPeriod).then(function (items) {
-            $scope.rankingList = items;
+            statisticsService.getRankingList(currentPeriod).then(function (items) {
+                $scope.rankingList = items;
+                extractFirstThreeRanks(items, $scope.podium);
+            });
+        }
 
-            // ASSUME SORTED INPUT; BACKEND PROVIDES IT
-            angular.forEach(items, function (rankListItem) {
-                if (rankListItem.rank > 3){
-                    return;
-                }
-                if (this[rankListItem.rank] === undefined){
-                    this[rankListItem.rank] = [];
-                }
-                this[rankListItem.rank].push(rankListItem.user);
-            }, $scope.podium);
-        });
+        init();
     }
 ]);
