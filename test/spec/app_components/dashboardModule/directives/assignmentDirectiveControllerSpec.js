7540a3ce1b92ec3052eb4108b63cfd0c1082a61d
Marco Reinwarth <marco.reinwarth@devbliss.com>
2015-05-19 15:53:03 +0200

    added tests

--- test/spec/app_components/dashboardModule/directives/assignmentDirectiveControllerSpec.js
+++ test/spec/app_components/dashboardModule/directives/assignmentDirectiveControllerSpec.js
@@ -8,17 +8,18 @@ describe('assignmentDirectiveController', function () {
         element,
         directiveScope;
 
-    function getDirectiveHtml(assigneeId) {
-        var pullRequestJson = '{}';
-        
-        if (assigneeId) {
-            pullRequestJson = '{assignee: {id: ' + assigneeId + ', fullName: \'user name\'}, elders: []}';
-        } else {
-            pullRequestJson = '{elders: []}';
-        }
-
-        return '<div data-dvb-assignment data-pull-request="' + pullRequestJson + '" data-logged-in-user="{id: 1234}"></div>';
+    function getDirectiveHtmlWithAnElder() {
+        return '<div data-dvb-assignment data-pull-request="{elders: [ {} ]}" data-logged-in-user="{id: 1234}"></div>';
     }
+
+    function getDirectiveHtmlWithAssignee(assigneeId) {
+        return '<div data-dvb-assignment data-pull-request="{assignee: {id: ' + assigneeId + ', fullName: \'user name\'}, elders: []}" data-logged-in-user="{id: 1234}"></div>';
+    }
+
+    function getDirectiveHtmlWithNoData() {
+        return '<div data-dvb-assignment data-pull-request="{elders: []}" data-logged-in-user="{id: 1234}"></div>';
+    }
+
     beforeEach(function () {
         module('userModule');
         module('dashboardModule');
@@ -34,42 +35,56 @@ describe('assignmentDirectiveController', function () {
 
     describe('init', function () {
         it('sets default settings because no assignee given', function () {
-            element = $compile(angular.element(getDirectiveHtml()))($scope);
+            element = $compile(angular.element(getDirectiveHtmlWithNoData()))($scope);
             $scope.$digest();
             directiveScope = element.isolateScope();
 
             expect(directiveScope.assignTitle).toEqual('dashboard.pullRequest.assign.toMe');
             expect(directiveScope.assignment).toEqual('assignToMe');
             expect(directiveScope.assignmentStyle).toEqual('');
+            expect(directiveScope.elderCheckModal).toEqual('');
+        });
+
+        it('sets an older pull request but still no assignee given', function () {
+            element = $compile(angular.element(getDirectiveHtmlWithAnElder()))($scope);
+            $scope.$digest();
+            directiveScope = element.isolateScope();
+
+            expect(directiveScope.assignTitle).toEqual('dashboard.pullRequest.assign.toMe');
+            expect(directiveScope.assignment).toEqual('confirmAssignToMe');
+            expect(directiveScope.assignmentStyle).toEqual('');
+            expect(directiveScope.elderCheckModal).toEqual('modal');
         });
 
         it('sets isAssignedToMe because logged in user is assigned', function () {
-            element = $compile(angular.element(getDirectiveHtml(1234)))($scope);
+            element = $compile(angular.element(getDirectiveHtmlWithAssignee(1234)))($scope);
             $scope.$digest();
             directiveScope = element.isolateScope();
 
             expect(directiveScope.assignTitle).toEqual('dashboard.pullRequest.assign.unassign');
             expect(directiveScope.assignment).toEqual('unassignMe');
             expect(directiveScope.assignmentStyle).toEqual('isAssignedToMe');
+            expect(directiveScope.elderCheckModal).toEqual('');
         });
 
         it('sets isAssigned because anyone else is assigned', function () {
-            element = $compile(angular.element(getDirectiveHtml(4321)))($scope);
+            element = $compile(angular.element(getDirectiveHtmlWithAssignee(4321)))($scope);
             $scope.$digest();
             directiveScope = element.isolateScope();
 
             expect(directiveScope.assignTitle).toEqual('dashboard.pullRequest.assign.toMe');
             expect(directiveScope.assignment).toEqual('confirmAssignToMe');
             expect(directiveScope.assignmentStyle).toEqual('isAssigned');
-       }); 
+            expect(directiveScope.elderCheckModal).toEqual('');
+        });
     });
-    
+
     describe('assignmentAction without modal', function () {
         beforeEach(function () {
             spyOn(pullRequestService, 'unassignPullRequest').and.returnValue(true);
             spyOn(pullRequestService, 'assignPullRequest').and.returnValue(true);
 
-            element = $compile(angular.element(getDirectiveHtml()))($scope);
+            element = $compile(angular.element(getDirectiveHtmlWithNoData()))($scope);
             $scope.$digest();
             directiveScope = element.isolateScope();
         });
@@ -90,15 +105,15 @@ describe('assignmentDirectiveController', function () {
             expect(pullRequestService.unassignPullRequest).toHaveBeenCalledWith(123);
         });
     });
-    
+
     describe('assignmentAction with modal and confirm assign to me', function () {
         beforeEach(function () {
             spyOn(pullRequestService, 'assignPullRequest').and.returnValue(true);
 
-            element = $compile(angular.element(getDirectiveHtml(4321)))($scope);
+            element = $compile(angular.element(getDirectiveHtmlWithAssignee(4321)))($scope);
             $scope.$digest();
             directiveScope = element.isolateScope();
-            
+
             var pr = {id: 123};
             directiveScope.assignmentAction(pr, 'confirmAssignToMe');
 
@@ -109,6 +124,11 @@ describe('assignmentDirectiveController', function () {
         it('assigns the pull request because confirmAssignment was called', function () {
             directiveScope.confirmAssignment();
             expect(pullRequestService.assignPullRequest).toHaveBeenCalledWith(123);
+        });
+
+        it('assigns another pull request instead the confirmed one because confirmOtherAssignment was called', function () {
+            directiveScope.confirmOtherAssignment({id: 234});
+            expect(pullRequestService.assignPullRequest).toHaveBeenCalledWith(234);
         });
     });
 });
