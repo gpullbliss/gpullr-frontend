0b792787cd2c2f98ce21398dc706e754bde2256d
Marco Reinwarth <marco.reinwarth@devbliss.com>
2015-05-20 16:29:07 +0200

    copy'n'paste grunt configuration from grunt-devbliss-conventions module

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,6 @@
 
 module.exports = function (grunt) {
     require('load-grunt-tasks')(grunt);
-    grunt.loadNpmTasks('grunt-devbliss');
 
     grunt.initConfig({
         connect: {
@@ -18,7 +17,74 @@ module.exports = function (grunt) {
                 rewrite: {
                     '^/api': ''
                 }
-            }]
+            }],
+            options: {
+                port: 8889,
+                // Change this to '0.0.0.0' to access the server from outside.
+                hostname: '0.0.0.0',
+                middleware: function (connect, options) {
+                    var middlewares = [];
+                    if (!Array.isArray(options.base)) {
+                        options.base = [options.base];
+                    }
+
+                    // Setup the proxy Backend
+                    var proxyOptions1 = require('url').parse('http://172.17.42.1:8070/api');
+                    proxyOptions1.route = '/api';
+                    middlewares.push(require('proxy-middleware')(proxyOptions1));
+
+                    // Setup the proxy qti player
+                    var proxyOptions2 = require('url').parse('http://localhost:13771/');
+                    proxyOptions2.route = '/qtiplayer';
+                    middlewares.push(require('proxy-middleware')(proxyOptions2));
+
+
+                    // RewriteRules support
+                    middlewares.push(require('grunt-connect-rewrite/lib/utils').rewriteRequest);
+
+                    // Serve static files
+                    options.base.forEach(function (base) {
+                        middlewares.push(connect.static(base));
+                    });
+                    return middlewares;
+                }
+            },
+
+            app: {
+                options: {
+                    open: true,
+                    livereload: 35729,
+                    base: ['.tmp', 'app']
+                }
+            },
+
+            dist: {
+                options: {
+                    open: true,
+                    base: 'dist'
+                }
+            },
+
+            testApp: {
+                options: {
+                    port: 8888,
+                    base: ['.tmp', 'test', 'app']
+                }
+            },
+
+            testDist: {
+                options: {
+                    port: 8888,
+                    base: ['.tmp', 'test', 'dist']
+                }
+            },
+
+            e2eApp: {
+                options: {
+                    port: 8888,
+                    base: ['.tmp', 'test', 'app']
+                }
+            }
         },
 
         jshint: {
@@ -32,9 +98,6 @@ module.exports = function (grunt) {
                 configFile: 'test/karma.conf.js'
             },
             app: {
-                browsers: ['PhantomJS']
-            },
-            jenkins: {
                 autoWatch: false,
                 browsers: ['PhantomJS'],
                 singleRun: true
@@ -42,6 +105,77 @@ module.exports = function (grunt) {
         },
 
         copy: {
+            components: {
+                files: [
+                    {
+                        expand: true,
+                        dot: true,
+                        cwd: 'app',
+                        dest: 'dist',
+                        src: [
+                            '.htaccess',
+                            'bower_components/**/*.html',
+                            'app_components/**/*.html',
+                            'views/**/*.html',
+                            'index.html'
+                        ]
+                    }
+                ]
+            },
+            js: {
+                files: [
+                    {
+                        expand: true,
+                        dot: true,
+                        cwd: '.tmp/concat/scripts/',
+                        dest: 'dist/scripts/',
+                        src: ['*.js']
+                    }
+                ]
+            },
+            font: {
+                files: [
+                    {
+                        expand: true,
+                        dot: true,
+                        cwd: 'app',
+                        src: [
+                            'styles/css/bootstrap/fonts/**/*',
+                            'styles/fonts/**/*',
+                            'bower_components/ecosystem-main-fe/release/styles/bootstrap/fonts/**/*'
+                        ],
+                        dest: 'dist'
+                    }
+                ]
+            },
+            img: {
+                files: [
+                    {
+                        expand: true,
+                        dot: true,
+                        cwd: 'app',
+                        src: [
+                            'bower_components/ecosystem-main-fe/release/images/**/*',
+                            'images/placeholder/*',
+                            'styles/img/**/*'
+                        ],
+                        dest: 'dist'
+                    }
+                ]
+            },
+            release: {
+                files: [
+                    {
+                        expand: true,
+                        dot: true,
+                        cwd: 'app/app_components/pageModule/',
+                        dest: 'release/',
+                        src: [
+                            '**/*'
+                        ]
+                    }
+                ]
+            },
             iconfont: {
                 expand: true,
                 cwd: 'app/styles/fonts',
@@ -85,11 +219,256 @@ module.exports = function (grunt) {
                     dest: 'app/scripts/config/'
                 }]
             }
+        },
+
+        wiredep: {
+            task: {
+                // Point to the files that should be updated when
+                // you run `grunt wiredep`
+                src: [
+                    'app/index.html'
+                ]
+            }
+        },
+
+        clean: {
+            dist: {
+                files: [
+                    {
+                        dot: true,
+                        src: [
+                            '.tmp',
+                            'app/styles/css/base.min.css',
+                            'dist/*',
+                            '!dist/.git*'
+                        ]
+                    }
+                ]
+            },
+            e2eDist: {
+                files: [
+                    {
+                        dot: true,
+                        src: [
+                            'dist/app_dev_components',
+                            'dist/bower_components',
+                            'dist/index_e2e.html'
+                        ]
+                    }
+                ]
+            },
+            release: {
+                files: [
+                    {
+                        dot: true,
+                        src: [
+                            'release/*'
+                        ]
+                    }
+                ]
+            }
+        },
+
+        recess: {
+            options: {
+                compile: true
+            },
+            dist: {
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'app/styles/less',
+                        src: 'base.less',
+                        dest: 'app/styles/.tmp/',
+                        ext: '.css'
+                    }
+                ]
+            }
+        },
+
+        eslint: {
+            target: [
+                'Gruntfile.js',
+                'app/scripts/**/*.js',
+                'app/app_components/**/*.js',
+                'app/app_dev_components/**/*.js'
+            ],
+            test: {
+                src: ['test/**/*.js']
+            },
+            options: {
+                configFile: 'config/eslint.json'
+            }
+        },
+
+        usemin: {
+            html: ['dist/index.html'],
+            options: {
+                assetsDirs: ['dist']
+            }
+        },
+
+        useminPrepare: {
+            html: 'app/index.html',
+            options: {
+                dest: 'dist'
+            }
+        },
+
+        rev: {
+            options: {
+                encoding: 'utf8',
+                algorithm: 'md5',
+                length: 4
+            },
+            dist: {
+                files: {
+                    src: [
+                        'dist/scripts/**/*.js',
+                        'dist/styles/**/*.css'
+                    ]
+                }
+            }
+        },
+
+        htmlmin: {
+            dist: {
+                options: {
+                    collapseWhitespace: true,
+                    collapseBooleanAttributes: true,
+                    removeCommentsFromCDATA: true,
+                    removeOptionalTags: true
+                },
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'dist',
+                        src: ['**/*.html'],
+                        dest: 'dist'
+                    }
+                ]
+            }
+        },
+
+        configureRewriteRules: {
+            connect: {
+                rules: [{
+                    from: '^/lat/$',
+                    to: 'http://0.0.0.0:8096/',
+                    redirect: 'permanent'
+                }, {
+                    from: '^/edi/$',
+                    to: 'http://0.0.0.0:8084/',
+                    redirect: 'permanent'
+                }, {
+                    from: '^/prep/$',
+                    to: 'http://0.0.0.0:8087/',
+                    redirect: 'permanent'
+                }, {
+                    from: '^/monitoring/(.*)$',
+                    to: 'http://0.0.0.0:8082',
+                    redirect: 'permanent'
+                }]
+            }
+        },
+
+        watch: {
+
+            // application files
+
+            html: {
+                files: ['app/**/*.html'],
+                options: {
+                    livereload: 35729
+                }
+            },
+
+            js: {
+                files: ['app/**/*.js'],
+                tasks: ['eslint'],
+                options: {
+                    livereload: 35729
+                }
+            },
+
+            less: {
+                files: ['app/styles/less/**/*.less'],
+                tasks: ['clean', 'recess'],
+                options: {
+                    livereload: 35729
+                }
+            },
+
+            css: {
+                files: ['app/**/*.css'],
+                options: {
+                    livereload: 35729
+                }
+            },
+
+            // tests
+
+            jsTest: {
+                files: ['test/**/*.js'],
+//                tasks: ['eslint:test'] TODO
+            },
+
+            // setup
+
+            bower: {
+                files: ['bower.json'],
+
+                tasks: ['wiredep']
+            },
+
+            gruntfile: {
+                files: ['Gruntfile.js'],
+//                tasks: ['eslint'], TODO
+                options: {
+                    livereload: 35729
+                }
+            }
         }
+
     });
 
-    grunt.registerTask('test', ['replace:development', 'devbliss-karma:jenkins']);
-    grunt.registerTask('buildpullr', ['replace:production', 'build', 'copy:iconfont']);
-    grunt.task.renameTask('serve', 'devbliss-serve');
-    grunt.registerTask('serve', ['build', 'replace:development', 'devbliss-serve']);
+    grunt.registerTask('buildpullr', [
+        'build'
+    ]);
+
+    grunt.registerTask('build', [
+        'replace:production',
+        'wiredep',
+        'clean',
+        'recess',
+//      'eslint', TODO
+        'useminPrepare',
+        'copy:components',
+        'copy:font',
+        'copy:img',
+        'concat',
+//      'uglify',
+        'cssmin',
+        'copy:js',
+        'rev:dist',
+        'usemin',
+//      'unit-test-dist',
+//      'e2e-test-dist',
+        'htmlmin',
+        'copy:iconfont'
+    ]);
+
+    grunt.registerTask('serve', [
+        'build',
+        'replace:development',
+        'configureRewriteRules',
+        'connect:app',
+        'watch'
+    ]);
+
+
+    grunt.registerTask('test', [
+        'replace:development',
+        'karma:app'
+    ]);
 };
