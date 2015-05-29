27f3f11af214184105111a86de85cff7e2f89d03
Daniel Walldorf <daniel.walldorf@devbliss.com>
2015-05-29 08:56:40 +0200

    review comments

--- app/app_components/dashboardModule/config/routeConfig.js
+++ app/app_components/dashboardModule/config/routeConfig.js
@@ -1,9 +1,8 @@
 'use strict';
 angular.module('dashboardModule')
-    .constant('STATE_DASHBOARD', 'dashboard')
     .constant('STATE_STATS', 'stats')
     .config([
-        '$stateProvider', 'STATE_DASHBOARD', 'STATE_STATS', function ($stateProvider, STATE_DASHBOARD, STATE_STATS) {
+        '$stateProvider', 'STATE_STATS', function ($stateProvider, STATE_STATS) {
             $stateProvider
                 .state(STATE_STATS, {
                     parent: 'page',
