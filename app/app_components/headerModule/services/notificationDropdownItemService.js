57cb97db61e876063db99013782e154739372a8c
Marco Reinwarth <marco.reinwarth@devbliss.com>
2015-05-29 09:02:45 +0200

    send desktop notification WIP

--- app/app_components/headerModule/services/notificationDropdownItemService.js
+++ app/app_components/headerModule/services/notificationDropdownItemService.js
@@ -1,6 +1,25 @@
 'use strict';
 angular.module('headerModule')
     .factory('notificationDropdownItemService', ['$filter', function ($filter) {
+
+        function sendNotification(title, message) {
+            if ('Notification' in window) {
+                Notification.requestPermission(function() {
+                    var options = {
+                        body: message,
+                        tag: 'preset',
+                        icon: 'http://gpullr.devbliss.com/favicon.ico'
+                    }
+
+                    var notification = new Notification(title, options);
+
+                    notification.onshow = function() {
+                        console.log('Notification shown');
+                    };
+                });
+            }
+        }
+
         function convert(n) {
             var text = '',
                 closedPr = $filter('translate')('navi.notifications.closedPr'),
@@ -9,6 +28,7 @@ angular.module('headerModule')
             switch (n.type) {
                 case 'PULLREQUEST_CLOSED':
                     text = n.actorName + ' ' + closedPr + ' ' + n.pullRequestTitle + ' ' + inRepo + ' ' + n.repoTitle + '"';
+                    sendNotification(closedPr, text);
                     break;
             }
 
