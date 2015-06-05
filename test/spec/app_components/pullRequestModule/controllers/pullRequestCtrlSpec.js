dd47bf25a01272cc0898abd984b3a4bbb144a7f1
Daniel Walldorf <daniel.walldorf@devbliss.com>
2015-06-05 09:37:33 +0200

    tests - WIP

--- test/spec/app_components/pullRequestModule/controllers/pullRequestCtrlSpec.js
+++ test/spec/app_components/pullRequestModule/controllers/pullRequestCtrlSpec.js
@@ -2,4 +2,17 @@
 
 describe('pullRequestCtrl', function () {
 
+    beforeEach(function () {
+        module('pullRequestModule');
+        module('angularMoment');
+    });
+
+    describe('assignment action', function () {
+
+        it('assign to me', function () {
+            fail('write test');
+        });
+
+    });
+
 });
