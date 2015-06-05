3e74328cff6d6a13194517589b0ca67ffe92520c
Daniel Walldorf <daniel.walldorf@devbliss.com>
2015-06-05 10:50:16 +0200

    everything working ecxept for tests

--- app/app_components/pullRequestModule/controllers/pullRequestCtrl.js
+++ app/app_components/pullRequestModule/controllers/pullRequestCtrl.js
@@ -4,31 +4,24 @@ angular.module('pullRequestModule')
     .controller('pullRequestCtrl', ['$rootScope', '$scope', 'userService', 'PullRequestService', 'PullRequestCssClassService',
         function ($rootScope, $scope, userService, pullRequestService, pullRequestCssClassService) {
 
+            var ACTION_ASSIGN_TO_ME = 'assignToMe',
+                ACTION_UNASSIGN_ME = 'unassignMe',
+                ACTION_CONFIRM_ELDER = 'confirmElder',
+                ACTION_CONFIRM_OVERRIDE_ASSIGNEE = 'confirmOverrideAssignee';
+
             $scope.getName = userService.getName;
             $scope.getColorClassDependingOnAge = pullRequestCssClassService.getColorClassDependingOnAge;
             $scope.getAbbreviateLines = pullRequestService.getAbbreviateLines;
 
-            var ACTION_ASSIGN_TO_ME = 'assignToMe',
-                ACTION_UNASSIGN_ME = 'unassignMe',
-                ACTION_CONFIRM_ASSIGN_TO_ME = 'confirmAssignToMe',
-                ACTION_OPEN_MODAL = 'modal',
-                defaultTitle = 'dashboard.pullRequest.assign.toMe';
-
             function determineAction(pullRequest) {
                 if (!pullRequest.assignee && pullRequest.elders.length === 0) {
-                    $scope.assignTitle = defaultTitle;
                     return ACTION_ASSIGN_TO_ME;
                 } else if (!pullRequest.assignee && pullRequest.elders.length > 0) {
-                    $scope.assignTitle = defaultTitle;
-                    $scope.elderCheckModal = ACTION_OPEN_MODAL;
-                    return ACTION_CONFIRM_ASSIGN_TO_ME;
+                    return ACTION_CONFIRM_ELDER;
                 } else if (pullRequest.assignee.id === $rootScope.user.id) {
-                    $scope.assignTitle = 'dashboard.pullRequest.assign.unassign';
                     return ACTION_UNASSIGN_ME;
                 } else {
-                    $scope.assignTitle = defaultTitle;
-                    $scope.assignmentModal = ACTION_OPEN_MODAL;
-                    return ACTION_CONFIRM_ASSIGN_TO_ME;
+                    return ACTION_CONFIRM_OVERRIDE_ASSIGNEE;
                 }
             }
 
@@ -49,9 +42,15 @@ angular.module('pullRequestModule')
 
                 switch (action) {
                     case ACTION_ASSIGN_TO_ME:
-                        pullRequestService.assignPullRequest(selectedPr.id);
+                        $scope.confirmAssignment(selectedPr);
                         break;
-                    case ACTION_CONFIRM_ASSIGN_TO_ME:
+                    case ACTION_CONFIRM_ELDER:
+                        $scope.selectedPullRequest = selectedPr;
+                        $scope.elderPullRequests = selectedPr.elders;
+                        angular.element('#confirm-elder-modal').modal('show');
+                        break;
+                    case ACTION_CONFIRM_OVERRIDE_ASSIGNEE:
+                        $scope.selectedPullRequest = selectedPr;
                         angular.element('#override-assignee-modal').modal('show');
                         break;
                     case ACTION_UNASSIGN_ME:
@@ -61,10 +60,13 @@ angular.module('pullRequestModule')
             };
 
             $scope.confirmAssignment = function (selectedPr) {
-                pullRequestService.assignPullRequest(selectedPr.id);
-            };
+                // for whatever reason, this modal (and only this) won't close otherwise...
+                angular.element('#confirm-elder-modal').modal('hide');
 
-            $scope.confirmOtherAssignment = function (selectedPr) {
+                // delete temporary vars
+                $scope.elderPullRequests = null;
+                $scope.selectedPullRequest = null;
+
                 pullRequestService.assignPullRequest(selectedPr.id);
             };
 
